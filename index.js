44f2a322ac3415c87114ed6afa38efc47b3a9487
Chandra Prakash Pal <chandu2013pal@gmail.com>
2020-01-23 07:10:42 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict'
+class User{
+	
+  // constructor(name1,name2){
+  //   this.fname=name1;
+  //   this.lname=name2;
+  // }
+	get name(){
+		return this._name;
+	}
+	set name(value){
+		this._name=value;
+	}
+  sayHi(){
+    return 'My Name is:-'+this.name;
+  }
+}
+//console.log(User.prototype.sayHi);
+let myObj=new User();
+myObj.name='Chandra Prakash Pal';
+console.log(myObj.sayHi());
